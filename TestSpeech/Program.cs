5d31333819981dd4c766344462ad2c328268b458
Manuel Espinosa <106558272+manuelesvi@users.noreply.github.com>
2023-11-27 21:09:06 -0600

    Update Program.cs

--- TestSpeech/Program.cs
+++ TestSpeech/Program.cs
@@ -19,7 +19,8 @@ while (true)
 
 return; //exit
 
-static string GetSalutes(VoiceInfo vInfo, int anio) => vInfo.Culture.Name switch
+static string GetSalutes(VoiceInfo vInfo, int anio) 
+    => vInfo.Culture.Name switch
 {
     "es-ES" => $"¡Feliz Navidad y próspero año {anio} desde España!",
     "es-MX" => $"¡Feliz Navidad y próspero año {anio}!",
@@ -34,12 +35,14 @@ static string GetFrenchSalute() =>
         ? "bon nuit" // 9pm-4am -> noche(s) o dia(s)
         : "bonjour");
 
-static string ByeSalute(VoiceInfo vInfo, string name) => vInfo.Culture.Name switch
+static string ByeSalute(VoiceInfo vInfo, string name) 
+    => vInfo.Culture.Name switch
 {
     "es-MX" => $"¡Adiós {name}!",
     "es-ES" => $"Anda {name}, hasta pronto!",
     "fr-FR" or "fr-CA" => $"{GetFrenchSalute()} {name}",
-    "en-US" or _ => $"Bye {name}, have a wonderful " + ( // en-US by default (_)
+    "en-US" or _ => $"Bye {name}, have a wonderful "
+        + ( // en-US by default (_)
         DateTime.Now.Hour < 3 ? "night" :
         DateTime.Now.Hour < 10 ? "morning" :
         DateTime.Now.Hour < 17 ? "day" :
@@ -69,7 +72,6 @@ static PromptBuilder BuildSalutePrompt(VoiceInfo vInfo, int anio)
         "es-MX" => $"Hola {name}, ¿cómo te va?"
     };
 #pragma warning enable
-
     promptBuilder.AppendText(salute);
     promptBuilder.EndVoice();
 
@@ -86,6 +88,7 @@ static PromptBuilder BuildSalutePrompt(VoiceInfo vInfo, int anio)
         Rate = PromptRate.Slow,
         Volume = PromptVolume.Loud
     });
+    
     promptBuilder.AppendText("Muy bien don Quijote, ¡gracias!");
     //promptBuilder.AppendText("Muy bien don Quijote, ¡gracias!",
     //    PromptEmphasis.Moderate);
@@ -122,7 +125,8 @@ static PromptBuilder BuildSalutePrompt(VoiceInfo vInfo, int anio)
 
     promptBuilder.AppendBreak(PromptBreak.Large);
 
-    promptBuilder.StartStyle(new PromptStyle { Emphasis = PromptEmphasis.Reduced });
+    promptBuilder.StartStyle(new PromptStyle { 
+        Emphasis = PromptEmphasis.Reduced });
     promptBuilder.AppendText(ByeSalute(vInfo, name));
     promptBuilder.EndStyle();
 
@@ -139,7 +143,8 @@ static bool PrintMenu(SpeechSynthesizer synthesizer)
     var voiceOpt = new List<ValueTuple<int, VoiceInfo>>();
     foreach (var v in voices)
     {
-        Console.WriteLine($"{i++}.- {GetPersonName(v.VoiceInfo)} [{v.VoiceInfo.Culture.DisplayName}]");
+        Console.WriteLine(
+            $"{i++}.- {GetPersonName(v.VoiceInfo)} [{v.VoiceInfo.Culture.DisplayName}]");
         voiceOpt.Add((i, v.VoiceInfo));
         if (v.VoiceInfo.Culture.Name == "es-ES" 
             && Globals.Espania is null)
@@ -191,7 +196,8 @@ struct LanguageRegion
 {
     public string Region;
     public string Language;
-    public static LanguageRegion Create(CultureInfo cultureInfo) => new LanguageRegion
+    public static LanguageRegion Create(CultureInfo cultureInfo) 
+        => new LanguageRegion
     {
         Language = cultureInfo.TwoLetterISOLanguageName,
         Region = cultureInfo.Name
